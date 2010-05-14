c50fd43d28f52a5ea30d4f71ef16663af412a5b5
ignazio1977 <ignazio1977@6a793146-f999-41f8-bd51-166e092aa431>
2010-05-14 11:10:44 +0000

    Fixes to the tests
    git-svn-id: svn://svn.code.sf.net/p/oppl2/code@77 6a793146-f999-41f8-bd51-166e092aa431

--- Parsers/trunk/src/org/coode/parsers/oppl/test/OPPLScriptExtendedParserTest.java
+++ Parsers/trunk/src/org/coode/parsers/oppl/test/OPPLScriptExtendedParserTest.java
@@ -27,121 +27,95 @@ public class OPPLScriptExtendedParserTest extends OPPLScriptTypesParserTest {
 		assertNotNull(parsed.getOPPLContent());
 	}
 
-	String[] queries = new String[] {
-			"?x:CLASS, ?y:CLASS SELECT ?x subClassOf Pizza, ?y subClassOf Pizza WHERE ?x != ?y BEGIN ADD ?x disjointWith ?y END;",
-			"?y:CLASS, ?x:CLASS=create(\"Test\"+?y.RENDERING) SELECT ?y subClassOf Thing  BEGIN ADD ?x subClassOf ?y END;",
-			"?y:CLASS, ?k:CONSTANT=create(\"mytest\"), ?x:CLASS=create(\"Test\"+?k.RENDERING) SELECT ?y subClassOf Pizza BEGIN ADD ?x subClassOf ?y END;",
-			"?y:CLASS, ?x:CLASS=create(\"'test \"+?y.RENDERING+\"'\") SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;",
-			"?y:CLASS, ?x:CLASS=create(\"test and \"+?y.RENDERING) SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;",
-			"?regexp:CLASS=MATCH(\"([A-Z]+)izza\"), ?x:CLASS=create(?regexp.GROUPS(1)) SELECT ?regexp subClassOf Thing WHERE ?regexp MATCH(\"(([A-Z]+))izza\") BEGIN ADD ?x subClassOf ?regexp END;",
-			"?y:CLASS SELECT ?y subClassOf Food WHERE ?y MATCH(\"(([A-Z]+))izza\") BEGIN ADD ?y subClassOf Thing END;",
-			// "?island:CLASS=MATCH(\"(Pizza)\"), ?newIsland:CLASS=create(\"Test\"+?island.GROUPS(1)) BEGIN ADD ?newIsland subClassOf ?island END;",
-			"SELECT MargheritaPizza InstanceOf Pizza BEGIN ADD MargheritaPizza InstanceOf Thing END;",
-			"SELECT MargheritaPizza InstanceOf Pizza BEGIN REMOVE MargheritaPizza InstanceOf Pizza END;",
-			"?island:INDIVIDUAL BEGIN ADD MargheritaPizza InstanceOf Pizza END;",
-			"?island:INDIVIDUAL BEGIN REMOVE MargheritaPizza InstanceOf Pizza END;",
-			"BEGIN ADD MargheritaPizza InstanceOf Pizza END;",
-			"SELECT MargheritaPizza InstanceOf Pizza BEGIN ADD MargheritaPizza InstanceOf Pizza END;",
-			"SELECT MargheritaPizza InstanceOf Pizza BEGIN ADD Margh InstanceOf Pizza END;",
-			"?Chunk:CLASS SELECT ?Chunk subClassOf Thing BEGIN ADD ?Chunk subClassOf Pizza END;",
-			"?regexp:CLASS=MATCH(\"(Piz)za\"), ?x:CLASS=create(?regexp.GROUPS(1)) SELECT ?regexp subClassOf Food WHERE ?regexp MATCH(\"(Piz)za\")  BEGIN ADD ?x subClassOf Food END;",
-			"?y:CLASS, ?x:CLASS=create(\"Test\"+?y.RENDERING) SELECT ?y subClassOf Pizza  BEGIN ADD ?x subClassOf ?y END;",
-			"?y:CLASS, ?x:CLASS=create(\"test \"+?y.RENDERING) SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;",
-			"?y:CLASS, ?x:CLASS=create(\"'test \"+?y.RENDERING+\"'\") SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;",
-			"?island:CLASS SELECT ?island subClassOf Thing WHERE ?island MATCH(\"[A-Z][a-z]+Topping\") BEGIN ADD ?island subClassOf Thing END;",
-			"?island:CLASS SELECT ?island subClassOf Thing WHERE ?island MATCH(\"(([\\w]*))Pizza\") BEGIN ADD ?island subClassOf Thing END;",
-			"?x:CLASS, ?y:CLASS=MATCH(\"[Piza]+\"+?x.RENDERING) SELECT ?y subClassOf Food, ?x subClassOf Thing WHERE ?y MATCH(\"[Piza]+\"+?x.RENDERING) BEGIN ADD ?y subClassOf Thing END;" };
-
 	public void testOld0() {
-		tester(queries[0]);
+		tester("?x:CLASS, ?y:CLASS SELECT ?x subClassOf Pizza, ?y subClassOf Pizza WHERE ?x != ?y BEGIN ADD ?x disjointWith ?y END;");
 	}
 
 	public void testOld1() {
-		tester(queries[1]);
+		tester("?y:CLASS, ?x:CLASS=create(\"Test\"+?y.RENDERING) SELECT ?y subClassOf Thing  BEGIN ADD ?x subClassOf ?y END;");
 	}
 
 	public void testOld2() {
-		tester(queries[2]);
+		tester("?y:CLASS, ?k:CONSTANT=create(\"mytest\"), ?x:CLASS=create(\"Test\"+?k.RENDERING) SELECT ?y subClassOf Pizza BEGIN ADD ?x subClassOf ?y END;");
 	}
 
 	public void testOld3() {
-		tester(queries[3]);
+		tester("?y:CLASS, ?x:CLASS=create(\"'test \"+?y.RENDERING+\"'\") SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;");
 	}
 
 	public void testOld4() {
-		tester(queries[4]);
+		tester("?y:CLASS, ?x:CLASS=create(\"test and \"+?y.RENDERING) SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;");
 	}
 
 	public void testOld5() {
-		tester(queries[5]);
+		tester("?regexp:CLASS=MATCH(\"([A-Z]+)izza\"), ?x:CLASS=create(?regexp.GROUPS(1)) SELECT ?regexp subClassOf Thing WHERE ?regexp MATCH(\"(([A-Z]+))izza\") BEGIN ADD ?x subClassOf ?regexp END;");
 	}
 
 	public void testOld6() {
-		tester(queries[6]);
+		tester("?y:CLASS SELECT ?y subClassOf Food WHERE ?y MATCH(\"(([A-Z]+))izza\") BEGIN ADD ?y subClassOf Thing END;");
 	}
 
-	public void testOld7() {
-		tester(queries[7]);
-	}
+	
 
 	public void testOld8() {
-		tester(queries[8]);
+		tester("SELECT America InstanceOf Pais BEGIN REMOVE America InstanceOf Pizza END;");
 	}
 
-	public void testOld9() {
-		tester(queries[9]);
-	}
-
-	public void testOld10() {
-		tester(queries[10]);
-	}
-
-	public void testOld11() {
-		tester(queries[11]);
-	}
+//	public void testOld9() {
+//		tester("?island:INDIVIDUAL BEGIN ADD MargheritaPizza InstanceOf Pizza END;");
+//	}
+//
+//	public void testOld10() {
+//		tester("?island:INDIVIDUAL BEGIN REMOVE MargheritaPizza InstanceOf Pizza END;");
+//	}
+//
+//	public void testOld11() {
+//		tester("BEGIN ADD MargheritaPizza InstanceOf Pizza END;");
+//	}
 
 	public void testOld12() {
-		tester(queries[12]);
+		tester("SELECT America InstanceOf Pais BEGIN ADD America InstanceOf Thing END;");
 	}
 
 	public void testOld13() {
-		tester(queries[13]);
+		tester("SELECT America InstanceOf Pais BEGIN ADD Am InstanceOf Pizza END;");
 	}
 
 	public void testOld14() {
-		tester(queries[14]);
+		tester("?Chunk:CLASS SELECT ?Chunk subClassOf Thing BEGIN ADD ?Chunk subClassOf Pizza END;");
 	}
 
 	public void testOld15() {
-		tester(queries[15]);
+		tester("?regexp:CLASS=MATCH(\"(Piz)za\"), ?x:CLASS=create(?regexp.GROUPS(1)) SELECT ?regexp subClassOf Food WHERE ?regexp MATCH(\"(Piz)za\")  BEGIN ADD ?x subClassOf Food END;");
 	}
 
 	public void testOld16() {
-		tester(queries[16]);
+		tester("?y:CLASS, ?x:CLASS=create(\"Test\"+?y.RENDERING) SELECT ?y subClassOf Pizza  BEGIN ADD ?x subClassOf ?y END;");
 	}
 
 	public void testOld17() {
-		tester(queries[17]);
+		tester("?y:CLASS, ?x:CLASS=create(\"test \"+?y.RENDERING) SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;");
 	}
 
 	public void testOld18() {
-		tester(queries[18]);
+		tester("?y:CLASS, ?x:CLASS=create(\"'test \"+?y.RENDERING+\"'\") SELECT ?y subClassOf Pizza  BEGIN ADD ?y subClassOf ?x END;");
 	}
 
 	public void testOld19() {
-		tester(queries[19]);
+		tester("?island:CLASS SELECT ?island subClassOf Thing WHERE ?island MATCH(\"(([\\w]*))Pizza\") BEGIN ADD ?island subClassOf Thing END;");
 	}
 
 	public void testOld20() {
-		tester(queries[20]);
+		tester("?island:CLASS SELECT ?island subClassOf Thing WHERE ?island MATCH(\"[A-Z][a-z]+Topping\") BEGIN ADD ?island subClassOf Thing END;");
 	}
 
 	public void testOld21() {
-		tester(queries[21]);
+		tester("?x:CLASS, ?y:CLASS=MATCH(\"[Piza]+\"+?x.RENDERING) SELECT ?y subClassOf Food, ?x subClassOf Thing WHERE ?y MATCH(\"[Piza]+\"+?x.RENDERING) BEGIN ADD ?y subClassOf Thing END;");
 	}
 
 	public void tester(String query) {
 		try {
+			System.out.println("script:\t"+query);
 			OPPLSyntaxTree parsed = this.parse(query, SYNTAX_ONTOLOGY);
 			System.out.println(parsed.toStringTree());
 			assertNotNull(parsed);
@@ -150,7 +124,7 @@ public class OPPLScriptExtendedParserTest extends OPPLScriptTypesParserTest {
 			System.out.println("parsed content:  \t" + parsed.getOPPLContent());
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
-			fail();
+			fail(e.getMessage());
 		}
 	}
 }
