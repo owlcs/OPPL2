c9dc9a70362004b9efefc1b4cf1b332da6d5f16d
luigiiannone <luigiiannone@6a793146-f999-41f8-bd51-166e092aa431>
2010-08-15 17:11:25 +0000

    20100815 Fixed Entity References including characters that are not letters
    git-svn-id: svn://svn.code.sf.net/p/oppl2/code@139 6a793146-f999-41f8-bd51-166e092aa431

--- tags/PARSERS/OWL_API_2/src/org/coode/parsers/test/TestExpressionParsing.java
+++ tags/PARSERS/OWL_API_2/src/org/coode/parsers/test/TestExpressionParsing.java
@@ -38,7 +38,8 @@ import org.semanticweb.owl.model.OWLOntologyManager;
  * 
  */
 public class TestExpressionParsing extends TestCase {
-	private static OWLOntologyManager ONTOLOGY_MANAGER = OWLManager.createOWLOntologyManager();
+	private static OWLOntologyManager ONTOLOGY_MANAGER = OWLManager
+			.createOWLOntologyManager();
 	private final static SymbolTableFactory<SymbolTable> SYMBOL_TABLE_FACTORY = new SimpleSymbolTableFactory(
 			ONTOLOGY_MANAGER);
 	public static TreeAdaptor adaptor = new CommonTreeAdaptor() {
@@ -56,7 +57,8 @@ public class TestExpressionParsing extends TestCase {
 		}
 
 		@Override
-		public Object errorNode(TokenStream input, Token start, Token stop, RecognitionException e) {
+		public Object errorNode(TokenStream input, Token start, Token stop,
+				RecognitionException e) {
 			return new CommonErrorNode(input, start, stop, e);
 		}
 	};
@@ -64,9 +66,11 @@ public class TestExpressionParsing extends TestCase {
 	private SymbolTable symtab;
 	static {
 		try {
-			ONTOLOGY_MANAGER.loadOntologyFromPhysicalURI(URI.create("http://www.co-ode.org/ontologies/pizza/2007/02/12/pizza.owl"));
-			ONTOLOGY_MANAGER.loadOntology(ComprehensiveAxiomTestCase.class.getResource(
-					"syntaxTest.owl").toURI());
+			ONTOLOGY_MANAGER
+					.loadOntologyFromPhysicalURI(URI
+							.create("http://www.co-ode.org/ontologies/pizza/2007/02/12/pizza.owl"));
+			ONTOLOGY_MANAGER.loadOntology(ComprehensiveAxiomTestCase.class
+					.getResource("syntaxTest.owl").toURI());
 		} catch (OWLOntologyCreationException e) {
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
@@ -88,12 +92,13 @@ public class TestExpressionParsing extends TestCase {
 			nodes.setTokenStream(tokens); // where to find tokens
 			nodes.setTreeAdaptor(adaptor);
 			this.symtab.setErrorListener(this.errorListener);
-			ManchesterOWLSyntaxSimplify simplify = new ManchesterOWLSyntaxSimplify(nodes);
+			ManchesterOWLSyntaxSimplify simplify = new ManchesterOWLSyntaxSimplify(
+					nodes);
 			simplify.setTreeAdaptor(adaptor);
 			simplify.downup(tree);
 			nodes.reset();
-			ManchesterOWLSyntaxTypes types = new ManchesterOWLSyntaxTypes(nodes, this.symtab,
-					this.errorListener);
+			ManchesterOWLSyntaxTypes types = new ManchesterOWLSyntaxTypes(
+					nodes, this.symtab, this.errorListener);
 			types.downup(tree);
 			return (ManchesterOWLSyntaxTree) tree;
 		} catch (RecognitionException e) {
@@ -121,6 +126,12 @@ public class TestExpressionParsing extends TestCase {
 
 	public void testOWLClassDescription() {
 		ManchesterOWLSyntaxTree parsed = this.parse("Pizza and Pizza");
+		assertNotNull(parsed);
+		System.out.println(parsed.toStringTree());
+	}
+
+	public void testEntityReference() {
+		ManchesterOWLSyntaxTree parsed = this.parse("'Pizza (the italian)'");
 		assertNotNull(parsed);
 		System.out.println(parsed.toStringTree());
 	}
