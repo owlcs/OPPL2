89ee8e260d3f07626b6406c80fff2ac0c0efd516
luigiiannone <luigiiannone@6a793146-f999-41f8-bd51-166e092aa431>
2010-08-20 16:01:28 +0000

    20100820 Fixed the OPPL Lint and filters reporting all the OWLObjects for each ontology
    git-svn-id: svn://svn.code.sf.net/p/oppl2/code@148 6a793146-f999-41f8-bd51-166e092aa431

--- OPPLLint/src/org/coode/oppl/lint/OPPLLintScript.java
+++ OPPLLint/src/org/coode/oppl/lint/OPPLLintScript.java
@@ -14,15 +14,28 @@ import org.coode.oppl.Variable;
 import org.coode.oppl.bindingtree.BindingNode;
 import org.coode.oppl.exceptions.OPPLException;
 import org.coode.oppl.rendering.ManchesterSyntaxRenderer;
+import org.coode.oppl.utils.DefaultOWLObjectVisitorEx;
+import org.coode.oppl.utils.OWLObjectExtractor;
 import org.semanticweb.owl.lint.Lint;
 import org.semanticweb.owl.lint.LintException;
 import org.semanticweb.owl.lint.LintReport;
 import org.semanticweb.owl.lint.LintVisitor;
 import org.semanticweb.owl.lint.LintVisitorEx;
 import org.semanticweb.owl.lint.configuration.LintConfiguration;
+import org.semanticweb.owl.model.OWLAxiom;
+import org.semanticweb.owl.model.OWLClass;
+import org.semanticweb.owl.model.OWLConstant;
+import org.semanticweb.owl.model.OWLDataProperty;
+import org.semanticweb.owl.model.OWLDataType;
+import org.semanticweb.owl.model.OWLEntity;
+import org.semanticweb.owl.model.OWLIndividual;
 import org.semanticweb.owl.model.OWLObject;
+import org.semanticweb.owl.model.OWLObjectProperty;
+import org.semanticweb.owl.model.OWLObjectVisitorEx;
 import org.semanticweb.owl.model.OWLOntology;
 import org.semanticweb.owl.model.OWLOntologyManager;
+import org.semanticweb.owl.model.OWLTypedConstant;
+import org.semanticweb.owl.model.OWLUntypedConstant;
 
 import uk.ac.manchester.cs.owl.lint.commons.NonConfigurableLintConfiguration;
 import uk.ac.manchester.cs.owl.lint.commons.SimpleMatchBasedLintReport;
@@ -34,6 +47,75 @@ import uk.ac.manchester.cs.owl.lint.commons.SimpleMatchBasedLintReport;
  * 
  */
 public class OPPLLintScript implements Lint<OWLObject> {
+	private final class OntologyReferenceDetector extends
+			DefaultOWLObjectVisitorEx<Boolean> implements
+			OWLObjectVisitorEx<Boolean> {
+		private final OWLOntology ontology;
+		private final Set<OWLConstant> constants = new HashSet<OWLConstant>();
+
+		/**
+		 * @param ontology
+		 */
+		public OntologyReferenceDetector(OWLOntology ontology) {
+			if (ontology == null) {
+				throw new NullPointerException("The ontology cannot be null");
+			}
+			this.ontology = ontology;
+			for (OWLAxiom axiom : ontology.getAxioms()) {
+				this.constants.addAll(OWLObjectExtractor
+						.getAllOWLConstants(axiom));
+			}
+		}
+
+		@Override
+		protected Boolean doDefault(OWLObject object) {
+			return false;
+		}
+
+		@Override
+		public Boolean visit(OWLClass desc) {
+			return this.visitOWLEntity(desc);
+		}
+
+		@Override
+		public Boolean visit(OWLDataProperty property) {
+			return this.visitOWLEntity(property);
+		}
+
+		@Override
+		public Boolean visit(OWLObjectProperty property) {
+			return this.visitOWLEntity(property);
+		}
+
+		@Override
+		public Boolean visit(OWLIndividual individual) {
+			return this.visitOWLEntity(individual);
+		}
+
+		@Override
+		public Boolean visit(OWLDataType node) {
+			return this.visitOWLEntity(node);
+		}
+
+		@Override
+		public Boolean visit(OWLTypedConstant node) {
+			return this.visitOWLConstant(node);
+		}
+
+		@Override
+		public Boolean visit(OWLUntypedConstant node) {
+			return this.visitOWLConstant(node);
+		}
+
+		private Boolean visitOWLConstant(OWLConstant node) {
+			return this.constants.contains(node);
+		}
+
+		private boolean visitOWLEntity(OWLEntity entity) {
+			return this.ontology.getReferencedEntities().contains(entity);
+		}
+	}
+
 	private String name;
 	private final String description;
 	private final OPPLScript opplScript;
@@ -46,14 +128,16 @@ public class OPPLLintScript implements Lint<OWLObject> {
 	 * @param opplScript
 	 * @param returnVariable
 	 */
-	public OPPLLintScript(String name, OPPLScript opplScript, Variable returnVariable,
-			String explanationTemplate, String description, OWLOntologyManager ontologyManager,
+	public OPPLLintScript(String name, OPPLScript opplScript,
+			Variable returnVariable, String explanationTemplate,
+			String description, OWLOntologyManager ontologyManager,
 			boolean inferenceRequired) {
 		if (name == null) {
 			throw new NullPointerException("The name cannot be null");
 		}
 		if (opplScript == null) {
-			throw new NullPointerException("The OPPL Script cannot be null cannot be null");
+			throw new NullPointerException(
+					"The OPPL Script cannot be null cannot be null");
 		}
 		if (returnVariable == null) {
 			throw new NullPointerException("The return variable cannot be null");
@@ -62,10 +146,12 @@ public class OPPLLintScript implements Lint<OWLObject> {
 			throw new NullPointerException("The description cannot be null");
 		}
 		if (explanationTemplate == null) {
-			throw new NullPointerException("The explanation template cannot be null");
+			throw new NullPointerException(
+					"The explanation template cannot be null");
 		}
 		if (ontologyManager == null) {
-			throw new NullPointerException("The ontology manager cannot be null");
+			throw new NullPointerException(
+					"The ontology manager cannot be null");
 		}
 		this.name = name;
 		this.opplScript = opplScript;
@@ -79,10 +165,12 @@ public class OPPLLintScript implements Lint<OWLObject> {
 	public Set<OWLObject> getDetectedObjects(OWLOntology ontology,
 			OWLOntologyManager ontologyManager) throws OPPLException {
 		this.getOPPLScript().getQuery().execute();
-		Set<BindingNode> leaves = this.getOPPLScript().getConstraintSystem().getLeaves();
+		Set<BindingNode> leaves = this.getOPPLScript().getConstraintSystem()
+				.getLeaves();
 		Set<OWLObject> toReturn = new HashSet<OWLObject>();
 		for (BindingNode leaf : leaves) {
-			OWLObject assignmentValue = leaf.getAssignmentValue(this.getReturnVariable());
+			OWLObject assignmentValue = leaf.getAssignmentValue(this
+					.getReturnVariable());
 			toReturn.add(assignmentValue);
 		}
 		return toReturn;
@@ -104,11 +192,8 @@ public class OPPLLintScript implements Lint<OWLObject> {
 	@Override
 	public String toString() {
 		Formatter formatter = new Formatter();
-		formatter.format(
-				"%s; %s RETURN %s; %s ",
-				this.getName(),
-				this.getOPPLScript().render(),
-				this.getReturnVariable().getName(),
+		formatter.format("%s; %s RETURN %s; %s ", this.getName(), this
+				.getOPPLScript().render(), this.getReturnVariable().getName(),
 				this.getDescription());
 		return formatter.out().toString();
 	}
@@ -148,8 +233,10 @@ public class OPPLLintScript implements Lint<OWLObject> {
 	protected final String getExplanation(OWLObject object) {
 		Variable returnVariable = this.getReturnVariable();
 		this.getOPPLScript().getConstraintSystem().getLeaves();
-		ManchesterSyntaxRenderer manchesterSyntaxRenderer = this.getOPPLScript().getConstraintSystem().getOPPLFactory().getManchesterSyntaxRenderer(
-				this.getOPPLScript().getConstraintSystem());
+		ManchesterSyntaxRenderer manchesterSyntaxRenderer = this
+				.getOPPLScript().getConstraintSystem().getOPPLFactory()
+				.getManchesterSyntaxRenderer(
+						this.getOPPLScript().getConstraintSystem());
 		object.accept(manchesterSyntaxRenderer);
 		String toReturn = "IMPOSSIBLE TO GENERATE AN EXPLANATION FOR "
 				+ manchesterSyntaxRenderer.toString();
@@ -158,24 +245,30 @@ public class OPPLLintScript implements Lint<OWLObject> {
 		// there is no way to enforce that given the genericness of a script,
 		// hence we will limit to consider the first match when generating the
 		// explanation String
-		Iterator<BindingNode> iterator = this.getOPPLScript().getConstraintSystem().getLeaves().iterator();
+		Iterator<BindingNode> iterator = this.getOPPLScript()
+				.getConstraintSystem().getLeaves().iterator();
 		while (!found && iterator.hasNext()) {
 			BindingNode bindingNode = iterator.next();
-			found = bindingNode.getAssignmentValue(returnVariable).equals(object);
+			found = bindingNode.getAssignmentValue(returnVariable).equals(
+					object);
 			if (found) {
 				toReturn = this.getExplanationTemplate();
 				String replacement;
 				for (Variable v : this.getOPPLScript().getVariables()) {
-					manchesterSyntaxRenderer = this.getOPPLScript().getConstraintSystem().getOPPLFactory().getManchesterSyntaxRenderer(
-							this.getOPPLScript().getConstraintSystem());
-					OWLObject assignmentValue = bindingNode.getAssignmentValue(v);
+					manchesterSyntaxRenderer = this.getOPPLScript()
+							.getConstraintSystem().getOPPLFactory()
+							.getManchesterSyntaxRenderer(
+									this.getOPPLScript().getConstraintSystem());
+					OWLObject assignmentValue = bindingNode
+							.getAssignmentValue(v);
 					if (assignmentValue == null) {
 						replacement = "UNKNOWN VALUE FOR " + v.getName();
 					} else {
 						assignmentValue.accept(manchesterSyntaxRenderer);
 						replacement = manchesterSyntaxRenderer.toString();
 					}
-					toReturn = toReturn.replaceAll("\\" + v.getName(), replacement);
+					toReturn = toReturn.replaceAll("\\" + v.getName(),
+							replacement);
 				}
 			}
 		}
@@ -185,15 +278,22 @@ public class OPPLLintScript implements Lint<OWLObject> {
 	/**
 	 * @see org.semanticweb.owl.lint.Lint#detected(java.util.Set)
 	 */
-	public LintReport<OWLObject> detected(Collection<? extends OWLOntology> targets)
-			throws LintException {
-		LintReport<OWLObject> toReturn = new SimpleMatchBasedLintReport<OWLObject>(this);
+	public LintReport<OWLObject> detected(
+			Collection<? extends OWLOntology> targets) throws LintException {
+		LintReport<OWLObject> toReturn = new SimpleMatchBasedLintReport<OWLObject>(
+				this);
 		for (OWLOntology ontology : targets) {
 			Set<OWLObject> detectedObjects;
+			OntologyReferenceDetector referenceDetector = new OntologyReferenceDetector(
+					ontology);
 			try {
-				detectedObjects = this.getDetectedObjects(ontology, this.getOntologyManager());
+				detectedObjects = this.getDetectedObjects(ontology, this
+						.getOntologyManager());
 				for (OWLObject object : detectedObjects) {
-					toReturn.add(object, ontology, this.getExplanation(object));
+					if (object.accept(referenceDetector)) {
+						toReturn.add(object, ontology, this
+								.getExplanation(object));
+					}
 				}
 			} catch (OPPLException e) {
 				throw new LintException(e);
