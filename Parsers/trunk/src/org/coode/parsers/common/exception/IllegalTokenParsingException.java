0f25e89ceedde7995c5a16bd25b8bea016eee5c6
luigiiannone <luigiiannone@6a793146-f999-41f8-bd51-166e092aa431>
2010-10-19 15:36:55 +0000

    Implementation of common Error listeners (Error Collector and Quick Fail)
    git-svn-id: svn://svn.code.sf.net/p/oppl2/code@241 6a793146-f999-41f8-bd51-166e092aa431

---
+++ Parsers/trunk/src/org/coode/parsers/common/exception/IllegalTokenParsingException.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.coode.parsers.common.exception;
+
+/**
+ * @author Luigi Iannone
+ * 
+ */
+public class IllegalTokenParsingException extends ParsingException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2790580594673053303L;
+	private final String token;
+
+	/**
+	 * @param line
+	 * @param charPositionInLine
+	 * @param message
+	 * @param cause
+	 * @param token
+	 */
+	public IllegalTokenParsingException(String token, int line, int charPositionInLine,
+			String message) {
+		super(message, line, charPositionInLine);
+		this.token = token;
+	}
+
+	/**
+	 * @return the token
+	 */
+	public String getToken() {
+		return this.token;
+	}
+}
