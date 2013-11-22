76aec4a58637e8dabfb625056a2583841d6d786d
ignazio <ignazio.palmisano@gmail.com>
2013-11-22 08:02:28 +0000

    unnecessary variable removed

--- oppl2plugin/src/main/java/org/coode/oppl/protege/ui/OPPLMList.java
+++ oppl2plugin/src/main/java/org/coode/oppl/protege/ui/OPPLMList.java
@@ -6,17 +6,12 @@ import javax.swing.ListModel;
 import org.protege.editor.core.ui.list.MList;
 
 public class OPPLMList extends MList {
-    private DefaultListModel<Object> list;
-
     public final DefaultListModel<Object> getDefaultModel() {
         return (DefaultListModel<Object>) getModel();
     }
 
     @Override
     public void setModel(ListModel model) {
-        if (model instanceof DefaultListModel) {
-            list = (DefaultListModel<Object>) model;
-        }
         super.setModel(model);
     }
 
